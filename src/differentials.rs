c10d966f91f4915475d5e767cf552ec539ffe8d8
Christopher Rabotin <christopher.rabotin@gmail.com>
2021-06-07 11:24:13 -0600

    Switched from SVector to OVector as underlying struct
    This allows for easier use in complicated code bases like nyx
    
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- src/differentials.rs
+++ src/differentials.rs
@@ -1,5 +1,7 @@
 use super::Float;
-use na::{SMatrix, SVector};
+use na::allocator::Allocator;
+use na::{DefaultAllocator, Dim, DimName};
+use na::{OMatrix, OVector};
 use {Dual, DualN, One, Scalar, Zero};
 
 /// Evaluates the function using dual numbers to get the partial derivative at the input point
@@ -13,14 +15,19 @@ where
 
 // Extracts Jacobian matrix and function value from a vector of dual numbers
 #[inline]
-pub fn extract_jacobian_and_result<T: Scalar + Zero + Float, const DIM_IN: usize, const DIM_OUT: usize, const DIM_HYPER: usize>(
-    fx_dual: &SVector<DualN<T, DIM_HYPER>, DIM_OUT>,
-) -> (SVector<T, DIM_OUT>, SMatrix<T, DIM_OUT, DIM_IN>) {
+pub fn extract_jacobian_and_result<T: Scalar + Zero + Float, DimIn: Dim + DimName, DimOut: Dim + DimName, DimHyper: Dim + DimName>(
+    fx_dual: &OVector<DualN<T, DimHyper>, DimOut>,
+) -> (OVector<T, DimOut>, OMatrix<T, DimOut, DimIn>)
+where
+    DefaultAllocator:
+        Allocator<T, DimIn> + Allocator<T, DimOut> + Allocator<T, DimOut, DimIn> + Allocator<DualN<T, DimHyper>, DimOut> + Allocator<T, DimHyper>,
+    <DefaultAllocator as Allocator<T, DimHyper>>::Buffer: Copy,
+{
     let fx = super::vector_from_hyperspace(&fx_dual);
-    let mut grad = SMatrix::<T, DIM_OUT, DIM_IN>::zeros();
+    let mut grad = OMatrix::<T, DimOut, DimIn>::zeros();
 
-    for i in 0..DIM_OUT {
-        for j in 0..DIM_IN {
+    for i in 0..DimOut::dim() {
+        for j in 0..DimIn::dim() {
             grad[(i, j)] = fx_dual[i][j + 1];
         }
     }
